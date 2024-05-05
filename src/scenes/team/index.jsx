8e6b3e14d38dced80f1ac46456190a0a485b772b
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-05-05 10:49:52 +0000

    Changes to be committed: 	modified:   src/data/mockdata.js 	modified:   src/scenes/global/newSidebar.jsx 	modified:   src/scenes/team/index.jsx

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -2,18 +2,75 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../themes";
 import { mockDataTeam } from "../../data/mockdata";
-import { AdminPanelSettingsOutlinedIcon } from "@mui/icons-material/AdminPanelSettingsOutlined";
-import { LockOpenOutlinedIcon } from "@mui/icons-material/LockOpenOutlined";
-import { SecurityOutlinedIcon } from "@mui/icons-material/SecurityOutlined";
-import Header from ".../.../components/Header"
+import { AdminPanelSettingsOutlined } from "@mui/icons-material";
+import { LockOpenOutlined } from "@mui/icons-material";
+import { SecurityOutlined } from "@mui/icons-material";
+import Header from "../../components/header";
 
-export default function Team(){
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode)
+export default function Team() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-    return (
-        <Box>
-            <Header title={"TEAM"} subtitle={"Managing the team members"} />
-        </Box>
-    )
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "access",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent={"center"}
+            backgroundColor={
+                access === "admin" ? colors.greenAccent[500] : colors.greenAccent[700]
+            }
+            borderRadius={"4px"}
+          >
+            {access === "admin" && <AdminPanelSettingsOutlined/>}
+            {access === "user" && <LockOpenOutlined/>}
+            {access === "manager" && <SecurityOutlined/>}
+            <Typography color={colors.grey[100]} sx={{ml: "5px"}}>
+                {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box>
+      <Header title={"TEAM"} subtitle={"Managing the team members"} />
+      <Box>
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
 }
